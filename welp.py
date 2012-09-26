f9df50ef9a74759200e685a05e2ba2006dd0badd
WebBreacher <webbreacher@gmail.com>
2012-09-26 18:04:51 -0400

    Added error checking for # of args passed, regex matches for the User Agent and HTTP Methods and some other tweaks.

--- welp.py
+++ welp.py
@@ -10,7 +10,7 @@
 
 # TODO
 #
-# 1 - Come up with HELP/USAGE
+# 1 - Come up with HELP/USAGE -> import cli.app
 # 2 - Implement options
 #   --types (-t)
 #       a - default = search for everything
@@ -19,17 +19,19 @@
 #       s - SQLi
 #       u - UserAgent Strings
 #       x - XSS
+# 3 - When a match is found
+#   a - Create a record of the event by IP. Track all the things that IP did in that entry
+#       Including: first and last times/dates seen, all attacks performed, etc.
 
 
 import os,sys,re
 
-# Search the correct areas of the logs for each of these (REGEXs)
+# Constants with the strings we will look for in the logfile
 # UserAgents show (in general) in Apache access.log not error.log
 USER_AGENT_STRINGS = ["dirbuster", "nikto", "netsparker", "acunetix", "w3af",\
                         "burp"]
 
-# TODO - Methods if not from 127.0.0.1 - Apache access.log
-# Maybe search for stuff other than the normal get,post,put,...etc?
+# HTTP Methods that aren't used much
 HTTP_METHOD_LIST = ["head", "options", "track", "trace"]
 
 # TODO - Search after the ? in non-RESTful URLs
@@ -50,24 +52,37 @@ def findIt(line, line_counter, search_cat, search_strings):
     # Need to remove the "global" below and get it to work another way
     global matches
 
-    # Read in line of the log file and look for all needles
+    # Break down the line into components
+    line_regex_match = re.search('"([A-Z]{3,8}) \/.* HTTP.*" \d{3} \d+ ".*" "([A-Za-z].+)"', line)
+
+    if search_cat == 'HTTP Method':
+        # Regex for HTTP Method is the first group
+        line = line_regex_match.group(1)
+    elif search_cat == 'User Agent':
+        # Regexe for the User Agent is second group
+        line = line_regex_match.group(2)
+
+    # Look for all search_strings
     for search_string in search_strings:
-        # TODO - Use specific REGEX for the specific NEEDLE_TYPE
+        # TODO - Use specific REGEX for the specific search_cat
         if re.search(search_string, line, re.I):
             log_entry = (search_cat, search_string)
             matches.add(log_entry)
 
-            print "[+] Line %s contains the %s string: %s" % (line_counter, search_cat, search_string) #DEBUG
+            #print "[+] Line %s contains the %s string: %s" % (line_counter, search_cat, search_string) #DEBUG
+
 
 # Check how many command line args were passed and provide HELP msg if not right
-user_log_file=sys.argv[1]
-
+if len(sys.argv) == 2:
+    user_log_file=sys.argv[1]
+else:
+    sys.exit('\n[!!] Fatal Error. You need to enter in the logfile name such as: %s logfilename\n' % sys.argv[0])
 
 
 # Parse the command arguments to see if the user passed in which tests they wanted done
 # TODO - read in args for -t or --type and add those lists to the tests{}
 # For now, make a dictionary and lets do all tests
-tests = { 'useragent': USER_AGENT_STRINGS,
+tests = { 'useragents': USER_AGENT_STRINGS,
           'httpmethods': HTTP_METHOD_LIST,
           'sqlcommands': SQL_COMMAND_LIST,
           'xsscommands': XSS_COMMAND_LIST,
@@ -88,11 +103,15 @@ try:
     # Start pulling each line of the file then performs all analysis
     for line in log_file:
 
+        # If the line is from 127.0.0.1|localhost, ignore it
+        if re.search('^((127.0.0.1)|localhost)', line):
+            continue
+
         if 'httpmethods' in tests.keys():    # Test for HTTPMethods
             findIt(line, line_counter, "HTTP Method", tests["httpmethods"])
 
-        if 'useragent' in tests.keys():      # Test for UserAgents
-            findIt(line, line_counter, "User Agent", tests["useragent"])
+        if 'useragents' in tests.keys():      # Test for UserAgents
+            findIt(line, line_counter, "User Agent", tests["useragents"])
 
         if 'sqlcommands' in tests.keys():      # Test for SQL Injection attacks
             findIt(line, line_counter, "SQLi", tests["sqlcommands"])
@@ -116,4 +135,3 @@ try:
 except (IOError) :
     print "\n\nCan't read file ... Exiting."
     sys.exit(0)
-
