0e1155129ef4e9f1df8d19747ce74256e15e5838
WebBreacher <webbreacher@gmail.com>
2012-10-06 00:16:46 -0400

    OMG lots of changes
    More comments. Less comments. Stripped out the hard-coded XSS/SQLI and
    IRQ gfrep terms as they cause MUCH false positives. Added a bunch of
    TODOs. Created a regex and Functions for the apache error log.

--- welp.py
+++ welp.py
@@ -9,15 +9,9 @@ Authore:     Micah Hoffman
 
  1 - Come up with HELP/USAGE -> import cli.app
  2 - Implement options
-   --types (-t)
-       a - default = search for everything
-       i - IRC commands
-       m - HTTP Methods
-       s - SQLi
-       u - UserAgent Strings
-       x - XSS
  3 - Count up the number of events per IP and give stats
- 4 - Require the default_filter.xml php-ids file (ship with one) in the current dir
+ 4 - Need to parse Apache error.log
+ 5 - Count lines in log file then give status every 1% completed - use http://pypi.python.org/pypi/progressbar
 '''
 
 import os, sys, re, itertools, operator
@@ -34,14 +28,10 @@ USER_AGENT_STRINGS = [".nasl","absinthe","acunetix", "arachni","bilbo","black wi
 # HTTP Methods that aren't used much
 HTTP_METHOD_LIST = ["head", "options", "track", "trace"]
 
-# TODO - Search after the ? in non-RESTful URLs
-SQL_COMMAND_LIST = ["select", "union", "group", "benchmark", "null", "'--", "or 1=1"]
-
-XSS_COMMAND_LIST = ["alert", "xss", "ha.ckers.org"]
-
-IRC_COMMAND_LIST = ["Joined channel", "Port", "BOT", "Login", "flood", "ddos", "NICK", "ECHO", "PRIVMSG", "ADMIN", "AWAY",\
-                    "CONNECT", "KICK", "LIST", "MODE", "MOTD", "PING", "POMG", "QUIT", "SERVLIST", "SERVICE", "NAMES", "JOIN",\
-                    "INVITE", "INFO", "TRACE", "USERHOST", "WHO", "WHOIS", "VERSION"]
+# IRC Commands - Getting many false positives with this
+#IRC_COMMAND_LIST = ["Joined channel", "Port", "BOT", "Login", "flood", "ddos", "NICK", "ECHO", "PRIVMSG", "ADMIN", "AWAY",\
+#                    "CONNECT", "KICK", "LIST", "MODE", "MOTD", "PING", "POMG", "QUIT", "SERVLIST", "SERVICE", "NAMES", "JOIN",\
+#                    "INVITE", "INFO", "TRACE", "USERHOST", "WHO", "WHOIS", "VERSION"]
 
 # TODO - May wish to use http://docs.python.org/library/collections.html - elements(), most_common() and others
 # Create a list of dictionaries per http://www.developer.nokia.com/Community/Wiki/List_of_Dictionaries_in_Python
@@ -54,35 +44,52 @@ php_ids_rules = {}
 
 def findIt(line, line_counter, search_cat, search_strings):
 
-    #print "[-] Examining log line %s" % line_counter #DEBUG
+    # TODO - Determine what type of log it is from regex
+    log_type=2 #Hard-coded DEBUG
 
     # Break down the log_file line into components
     # TODO - Need to examine other web server logs (IIS, ColdFusion, Tomcat, ...)
-    # Apache 2.x access log regex
-    # 1=IP, 2=Date/Time of the activity, 3=HTTP Method, 4=URL Requested, 5=User Agent
-    line_regex_split = re.search('^(\d+\.\d+\.\d+\.\d+) .*\[(\d+.*) \-\d+\] "([A-Z]{1,11}) (\/.*) HTTP.*" \d{3} \d+ ".*" "([A-Za-z].+)"', line)
+    if log_type == 2:
+        # TODO - Make this strip off/ignore the referrer if it is there
+        # Apache 2.x error log is separated by []
+        line_regex_split = re.search('^\[([SMTWF].*)\] \[error\] \[client (\d.*)\] (.*)(, referer.*)', line)
 
-    # Assign easy to understand variables
-    remote_ip     = line_regex_split.group(1)
-    event_date    = line_regex_split.group(2)
-    http_method   = line_regex_split.group(3)
-    url_requested = line_regex_split.group(4)
-    user_agent    = line_regex_split.group(5)
+        # Some lines in the log we don't care about (notice, info...). Discard those lines
+        if line_regex_split == None: return
 
-    # Set the spot in the log entry that we want to examine
-    if search_cat == 'HTTP Method':
-        line = http_method          # Regex for HTTP Method is the first group
-    elif search_cat == 'User Agent':
-        line = user_agent           # Regex for the User Agent is second group
-    elif search_cat == 'IRC' or search_cat == 'XSS' or search_cat == 'SQLi':
-        line = url_requested        # Regex should look at the URL requested
+        # Assign easy to understand variables
+        remote_ip     = line_regex_split.group(2)
+        event_date    = line_regex_split.group(1)
+        error_thrown  = line_regex_split.group(3)
+        user_agent    = 'Error Log. No U/A'
 
-    # Look for search_strings
-    for search_string in search_strings:
-        if re.search(search_string, line, re.I):
-            # Add content to the attacker list of dictionaries
-            attacker.append({'ip': remote_ip,'user_agent': user_agent, 'event_date': event_date, 'cat': search_cat,
-            'string':search_string, 'line': line, 'line_number': line_counter})
+        # Set the spot in the log entry that we want to examine
+        line = error_thrown
+
+    else:
+        # Apache 2.x access log regex
+        # 1=IP, 2=Date/Time of the activity, 3=HTTP Method, 4=URL Requested, 5=User Agent
+        line_regex_split = re.search('^(\d+\.\d+\.\d+\.\d+) .*\[(\d+.*) \-\d+\] "([A-Z]{1,11}) (\/.*) HTTP.*" \d{3} \d+ ".*" "([A-Za-z].+)"', line)
+
+        # Assign easy to understand variables
+        remote_ip     = line_regex_split.group(1)
+        event_date    = line_regex_split.group(2)
+        http_method   = line_regex_split.group(3)
+        url_requested = line_regex_split.group(4)
+        user_agent    = line_regex_split.group(5)
+
+        # Set the spot in the log entry that we want to examine
+        if search_cat == 'HTTP Method':
+            line = http_method          # Regex for HTTP Method is the first group
+        elif search_cat == 'User Agent':
+            line = user_agent           # Regex for the User Agent is second group
+
+        # Look for search_strings
+        for search_string in search_strings:
+            if re.search(search_string, line, re.I):
+                # Add content to the attacker list of dictionaries
+                attacker.append({'ip': remote_ip,'user_agent': user_agent, 'event_date': event_date, 'cat': search_cat,
+                'string':search_string, 'line': line, 'line_number': line_counter})
 
     # Look for PHP-IDS matches
     for id in php_ids_rules.keys():
@@ -94,8 +101,8 @@ def findIt(line, line_counter, search_cat, search_strings):
 
         if regex.search(line):
             # Add content to the attacker list of dictionaries
-            attacker.append({'ip': remote_ip,'user_agent': user_agent, 'event_date': event_date, 'cat': "PHP-IDS Rule",
-            'string':php_ids_rules[id], 'line': line, 'line_number': line_counter})
+            attacker.append({'ip': remote_ip,'user_agent': user_agent, 'event_date': event_date, 'cat': 'PHP-IDS Rule',
+            'string': id, 'line': line, 'line_number': line_counter})
 
 def main():
 
@@ -111,15 +118,12 @@ def main():
 
     # TODO - Read in args for -t or --type and add those lists to the tests{}
     # For now, make a dictionary and lets do all tests
-    tests = { 'User Agent': USER_AGENT_STRINGS,
-              'HTTP Method': HTTP_METHOD_LIST,
-              'SQLi': SQL_COMMAND_LIST,
-              'XSS': XSS_COMMAND_LIST,
-              'IRC': IRC_COMMAND_LIST   }
+    tests = { 'User Agent': USER_AGENT_STRINGS, 'HTTP Method': HTTP_METHOD_LIST }
 
     # Open the log_file (or try to)
     try:
         log_file = open(user_log_file,'r').readlines()
+
     except (IOError) :
         sys.exit("\n[!!] Can't read file the logfile you entered.\
                   \n[!!] Exiting.\n")
@@ -142,6 +146,7 @@ def main():
         rule_content = rule_xml.replace('<rule>','').replace('</rule>','')
         rule_content = rule_content.replace("<![CDATA[", "")
         rule_content = rule_content.replace("]]>", "")
+        #TODO - Grab the rule name or what it does and output that for the "string" in the ouput
 
         try:
             regex = re.compile(rule_content)
@@ -151,13 +156,15 @@ def main():
 
         php_ids_rules[id_content] = rule_content
 
-    #print list(php_ids_rules.items()) #DEBUG
 
     # Actually start to look for stuff
     print "\n[-] Analyzing the file: ", user_log_file
+    print "[-] Processing line ",
 
     # Pull each line of the file then perform all analysis
     for line in log_file:
+        # Shows that we are actually working
+        print (line_counter),
 
         # If the log traffic is from 127.0.0.1|localhost, ignore it
         if re.search('^((127.0.0.1)|localhost)', line):
@@ -165,8 +172,12 @@ def main():
 
         # Cycle through each of the tests the user specified
         for key in tests:
+
             findIt(line, line_counter, key, tests[key])
             line_counter += 1
+
+            # TODO - Tell user how far into the file we are using http://pypi.python.org/pypi/progressbar
+
 
     # Show the Results
     if len(attacker) == 0:
