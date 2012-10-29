7359c53c45f531778b61f241dead88dee8e0a439
WebBreacher <webbreacher@gmail.com>
2012-10-29 17:28:12 -0400

    Got the attacker tracker and output looking good

--- welp.py
+++ welp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python -tt
 '''
 -------------------------------------------------------------------------------
 Name:        WELP - Web Error Log Processor
@@ -7,13 +7,6 @@ Author:      Micah Hoffman (@WebBreacher)
 -------------------------------------------------------------------------------
  TODO (Overall)
 
- 1 -
- 2 -
- 3 - Count up the number of events per IP and give stats
- 4 -
- 5 - Make output nicer looking and grouped by IP
- 6 - Revise the User Agent searching - Record a set of all User Agents and post process for apps/tools
- 7 -
  8 - Look for other anomalies such as known bad (RAT) strings (/w00t-w00t...)
  9 - Do analysis on the IPs found - lookup? Country?
  10- Look at the HTTP response code for success or failure and only report 2xx
@@ -23,7 +16,7 @@ Author:      Micah Hoffman (@WebBreacher)
 
 '''
 
-import os, sys, re, itertools, operator
+import os, sys, re, itertools, operator, signal
 from datetime import datetime
 from xml.dom import minidom
 from welpcore import *
@@ -38,31 +31,17 @@ USER_AGENT_STRINGS = [".nasl","absinthe","acunetix", "arachni","bilbo","black wi
 
 HTTP_METHOD_LIST = ["head", "options", "track", "trace"]
 
-# TODO - May wish to use http://docs.python.org/library/collections.html - elements(), most_common() and others
-# Create a list of dictionaries per http://www.developer.nokia.com/Community/Wiki/List_of_Dictionaries_in_Python
-attacker = [] # TODO - Change this to dictionary and make the evaluations about the attacker
+
+attacker = [] #ip,ua,date_earliest,date_recent,date_all,cats,attacks,lines
 php_ids_rules = {}
 log = {}
 
 #=================================================
 # Functions & Classes
 #=================================================
-
-class Attacker:
-    attacker_counter = 0
-
-    def __init__(self, ip):
-        self.ip = ip
-        self.user_agent = set([])
-        self.date_earliest_event = datetime.now()
-        self.date_recent_event = datetime.now()
-        Attacker.attacker_counter += 1
-
-    def date_work(self, event_date):
-        tt = datetime.strptime(event_date, "%d/%b/%Y:%H:%M:%S")
-        if self.date_earliest_event > tt : self.date_earliest_event = tt
-        if self.date_recent_event < tt : self.date_recent_event = tt
-
+def signal_handler(signal, frame):
+        print bcolors.RED + '\nYou pressed Ctrl+C! Exiting.' + bcolors.ENDC
+        sys.exit()
 
 def rematch(line):      # Determine log type and set name/regex
     # Apache 2.x Error Log
@@ -86,6 +65,35 @@ def rematch(line):      # Determine log type and set name/regex
     print bcolors.RED + "\n[Error] " + bcolors.ENDC + "No idea what kinda log you just submitted. Right now we only work on Apache 2.x access and error logs."
     sys.exit()
 
+def seen_ip_before(event): 
+    # 0=remote_ip,1=user_agent,2=event_date,3=search_cat,4=search_string,5=line,6=line_counter]
+    for actor in attacker:
+        if event[0] in actor['ip']:
+            print bcolors.YELLOW + "[Found] New activity for %s; Line# %d." % (event[0],event[6])
+            if event[1].find('Nikto'): event[1] = 'Nikto' #TODO just grab using regex the Nikto and version
+            actor['ua'].add(event[1])
+            tt = datetime.strptime(event[2], "%d/%b/%Y:%H:%M:%S")
+            actor['date_all'].add(tt)
+            actor['cats'].add(event[3])
+            if actor['date_earliest'] > tt : actor['date_earliest'] = tt
+            if actor['date_recent'] < tt : actor['date_recent'] = tt
+            actor['cats'].add(event[4])
+            actor['line_num'].add(event[6])
+            return
+
+    # Add new if we haven't had a match
+    print bcolors.PURPLE + "[Found] Making new record for %s." % event[0]
+    if event[1].find('Nikto'): event[1] = 'Nikto' #TODO just grab using regex the Nikto and version
+    attacker.append({'ip': event[0],\
+                     'ua': set([event[1]]),\
+                     'date_earliest':datetime.strptime(event[2], "%d/%b/%Y:%H:%M:%S"),\
+                     'date_recent':datetime.strptime(event[2], "%d/%b/%Y:%H:%M:%S"),\
+                     'date_all':set([datetime.strptime(event[2], "%d/%b/%Y:%H:%M:%S")]),\
+                     'cats':set([event[3]]),\
+                     'attacks':set([event[4]]),\
+                     'line_num':set([event[6]])\
+                     })
+    
 
 def findIt(line, line_counter, search_cat, search_strings):
 
@@ -124,9 +132,9 @@ def findIt(line, line_counter, search_cat, search_strings):
         # Look for search_strings
         for search_string in search_strings:
             if re.search(search_string, line, re.I):
-                # Add content to the attacker list of dictionaries
-                attacker.append({'ip': remote_ip,'user_agent': user_agent, 'event_date': event_date, 'cat': search_cat,
-                'string':search_string, 'line': line, 'line_number': line_counter})
+                # Add content to the attacker
+                event = [remote_ip,user_agent,event_date,search_cat,search_string,line,line_counter]
+                seen_ip_before(event)
 
     # Look for PHP-IDS matches
     for id in php_ids_rules.keys():
@@ -138,8 +146,8 @@ def findIt(line, line_counter, search_cat, search_strings):
 
         if regex.search(line):
             # Add content to the attacker list of dictionaries
-            attacker.append({'ip': remote_ip,'user_agent': user_agent, 'event_date': event_date, 'cat': 'PHP-IDS Rule',
-            'string': id, 'line': line, 'line_number': line_counter})
+            event = [remote_ip,user_agent,event_date,'PHP-IDS Rule',id,line,line_counter]
+            seen_ip_before(event)
 
 def main():
 
@@ -201,13 +209,15 @@ def main():
 
     # Actually start to look for stuff
     print bcolors.GREEN + "[info] " + bcolors.ENDC + "Analyzing the file:", user_log_file
-    print bcolors.GREEN + "[info] " + bcolors.ENDC+ "There are %d lines in this log file." % len(log_file)
+    print bcolors.GREEN + "[info] " + bcolors.ENDC+ "There are %d lines in this log file." % len(log_file) #TODO check this
 
     # Pull each line of the file then perform all analysis
     for line in log_file:
-        sys.stdout.write(bcolors.BLUE + "\r[info] " + bcolors.YELLOW + "Processing line # %d" %line_counter)
-        sys.stdout.flush()
+        #sys.stdout.write(bcolors.BLUE + "\r[info] " + bcolors.YELLOW + "Processing line # %d" %line_counter)
+        #sys.stdout.flush()
 
+        signal.signal(signal.SIGINT, signal_handler)    # Trap Ctrl-C
+        
         # If the log traffic is from 127.0.0.1|localhost, ignore it
         if re.search('^((127.0.0.1)|localhost)', line): continue
 
@@ -219,14 +229,20 @@ def main():
 
     # Show the Results
     if len(attacker) == 0:
-        print bcolors.PURPLE + "[info] " + bcolors.ENDC + "No strings found."
+        print bcolors.GREEN + "[info] " + bcolors.ENDC + "No security events found."
 
     elif len(attacker) > 0:
-        print bcolors.YELLOW + "[FOUND] " + bcolors.ENDC + "Found the following IPs (and associated activity)"
+        print bcolors.RED + "[FOUND] " + "Found the following IPs (and associated activity)" + bcolors.ENDC
 
-        attacker.sort(key=operator.itemgetter('string'))
+        #attacker.sort(key=operator.itemgetter('string'))
         for event in attacker:
-            print bcolors.YELLOW + '    [FOUND] ' + bcolors.ENDC + '"%s" found in IP: %s in line# %d -> %s' % (event['string'], event['ip'], event['line_number'], event['line']        )
+            print bcolors.YELLOW +  "%s" % event['ip']
+            print bcolors.ENDC +    "\tUser-Agents: %s" % ",\n\t\t".join(event['ua'])
+            print                   "\tEarliest Date Seen:   %s" % event['date_earliest']
+            print                   "\tEarliest Recent Seen: %s" % event['date_recent']
+            #print                   "\tAll Dates Seen:       %s" % ", ".join(event['date_all'])
+            print                   "\tAll Categories Seen:  %s" % ", ".join(event['cats'])
+            print                   "\tAll Attacks Seen:     %s" % ", ".join(event['attacks'])
 
 
 #=================================================
@@ -235,4 +251,4 @@ def main():
 
 if __name__ == "__main__": main()
 
-print bcolors.GREEN + "\n[Finished] " + bcolors.CYAN + "WELP script completed. Thanks for using it.\n"
+print bcolors.GREEN + "[Finished] " + bcolors.CYAN + "WELP script completed with %d events identified. Thanks for using it.\n" % len(attacker)
