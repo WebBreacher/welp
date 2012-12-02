a80319541ab3f4f3e87391aed32f9272fc50547d
WebBreacher <webbreacher@gmail.com>
2012-12-02 16:04:49 -0500

    Fixed spacing

--- welpcore.py
+++ welpcore.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python
-
 '''
-
 -------------------------------------------------------------------------------
-
 Name:        WELP Core Functions and Such
-
 Author:      Micah Hoffman (@WebBreacher)
-
 -------------------------------------------------------------------------------
-
-
-
 '''
-
-
 
 import os, sys
 
@@ -58,187 +48,96 @@ def signal_handler(signal, frame):
 # check operating system or if we have an output file, don't colorize text
 
 def check_os():
-
     if os.name == "nt":
-
         operating_system = "windows"
-
     if os.name == "posix":
-
         operating_system = "posix"
-
     return operating_system
 
-
-
 #
-
 # Class for colors
-
 #
-
 if check_os() == "posix":
-
     class bcolors:
-
         PURPLE = '\033[95m'
-
         CYAN = '\033[96m'
-
         DARKCYAN = '\033[36m'
-
         BLUE = '\033[94m'
-
         GREEN = '\033[92m'
-
         YELLOW = '\033[93m'
-
         RED = '\033[91m'
-
         BOLD = '\033[1m'
-
         UNDERL = '\033[4m'
-
         ENDC = '\033[0m'
-
         backBlack = '\033[40m'
-
         backRed = '\033[41m'
-
         backGreen = '\033[42m'
-
         backYellow = '\033[43m'
-
         backBlue = '\033[44m'
-
         backMagenta = '\033[45m'
-
         backCyan = '\033[46m'
-
         backWhite = '\033[47m'
 
-
-
         def disable(self):
-
             self.PURPLE = ''
-
             self.CYAN = ''
-
             self.BLUE = ''
-
             self.GREEN = ''
-
             self.YELLOW = ''
-
             self.RED = ''
-
             self.ENDC = ''
-
             self.BOLD = ''
-
             self.UNDERL = ''
-
             self.backBlack = ''
-
             self.backRed = ''
-
             self.backGreen = ''
-
             self.backYellow = ''
-
             self.backBlue = ''
-
             self.backMagenta = ''
-
             self.backCyan = ''
-
             self.backWhite = ''
-
             self.DARKCYAN = ''
 
-
-
 # if we are windows or something like that then define colors as nothing
-
 else:
-
     class bcolors:
-
         PURPLE = ''
-
         CYAN = ''
-
         DARKCYAN = ''
-
         BLUE = ''
-
         GREEN = ''
-
         YELLOW = ''
-
         RED = ''
-
         BOLD = ''
-
         UNDERL = ''
-
         ENDC = ''
-
         backBlack = ''
-
         backRed = ''
-
         backGreen = ''
-
         backYellow = ''
-
         backBlue = ''
-
         backMagenta = ''
-
         backCyan = ''
-
         backWhite = ''
 
-
-
         def disable(self):
-
             self.PURPLE = ''
-
             self.CYAN = ''
-
             self.BLUE = ''
-
             self.GREEN = ''
-
             self.YELLOW = ''
-
             self.RED = ''
-
             self.ENDC = ''
-
             self.BOLD = ''
-
             self.UNDERL = ''
-
             self.backBlack = ''
-
             self.backRed = ''
-
             self.backGreen = ''
-
             self.backYellow = ''
-
             self.backBlue = ''
-
             self.backMagenta = ''
-
             self.backCyan = ''
-
             self.backWhite = ''
-
             self.DARKCYAN = ''
 
 '''
